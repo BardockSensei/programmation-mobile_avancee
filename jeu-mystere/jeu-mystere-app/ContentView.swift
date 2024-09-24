70ee892c0575c8587d0ceeba39d89b2074d552d8
Guillaume Hostache <guillaume.hostache@outlook.fr>
2024-09-24 12:04:47 +0200

    tp2(jeu mystere interface): fin tp2

--- jeu-mystere/jeu-mystere-app/ContentView.swift
+++ jeu-mystere/jeu-mystere-app/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+func finDePartie(nombreTentatives: Int, reponse: Int) -> String {
+    if (nombreTentatives >= 17) {
+        return "Bravo, vous avez beaucoup de chance !";
+    } else if (nombreTentatives >= 14) {
+        return "Bravo, très bonne stratégie avec un peu de chance...";
+    } else if (nombreTentatives >= 9) {
+        return "Bravo, c'est bien !"
+    } else if (nombreTentatives > 0) {
+        return "Bien, mais vous pouvez mieux faire...";
+    } else {
+        return "Perdu !\nCe n'est pas de chance, le nombre mystère était \(reponse)...";
+    }
+}
+
 struct ContentView: View {
+    @State var nombreMystere :Int = Int.random(in: 1...100);
+    
     @State var nbJoue :String = "";
-    let nombreMystere = Int.random(in: 1...100)
     @State var indications :String = "";
-    //var victoire :Boolean = false;
     @State var nbTentatives :Int = 10;
+    @State var propositions :[Int] = [];
     
     var body: some View {
         VStack(spacing: 0) {
+            // Section titre base
             VStack {
                 Text("Hello Master CIM !!")
                     .font(.title)
@@ -23,58 +39,89 @@ struct ContentView: View {
                     .foregroundColor(Color.blue)
                     .padding([.bottom], 30)
                     .shadow(color: Color.black, radius: 10, x:15, y:20)
+                
                 Text("TP2 - Guillaume Hostache")
                     .font(.subheadline)
                     .padding([.all], 5)
-            }   .padding()
-                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight:0, maxHeight: 150, alignment: .top)
+            }.padding()
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 0, maxHeight: 150, alignment: .top)
                 .background(Color(.sRGB, white:0.95, opacity:1.0))
                 
-            // Hack pour avoir une barre bleue
-            VStack {}.frame(maxWidth: .infinity, maxHeight: 5)
-                .background(Color.blue)
+            // Hack pas fou pour avoir une barre bleue
+            VStack {}.frame(maxWidth: .infinity, maxHeight: 5).background(Color.blue)
             
+                //Section jeu
             VStack(alignment: .center) {
-                Text("Jeu du Nombre Mystère")
-                    .padding()
-                    .font(.title)
+                Text("Jeu du Nombre Mystère").padding().font(.title)
                 
-                Text("Nombres d'essais restants : ").padding()
+                Text("Il vous reste \(nbTentatives) essais").padding(5)
                 
+                ProgressView(value: Double(10-nbTentatives), total: 10)
+                        .padding([.top], 4)
+                        .padding([.bottom], 10)
+                    
                 TextField("...", text: $nbJoue)
                     .padding()
                     .background(Color(.sRGB, white:0.95, opacity:1.0))
                     .foregroundColor(.blue)
+                    .frame(maxWidth: 350)
                 
-                Button("Jouer") {
-                    print("TRICHE : \(nombreMystere)")
-                    let proposition :Int = Int(nbJoue) ?? -1
-                    print("Saisie : \(proposition)")
+                    HStack(spacing: 25) {
+                        Button("Jouer") {
+                        print("TRICHE : \(nombreMystere)");
+                        let proposition :Int = Int(nbJoue) ?? -1;
+                        if (proposition != -1) {
+                            if (propositions.contains(proposition)) {
+                                indications = "Vous avez déjà essayez ce nombre...";
+                            } else {
+                                nbTentatives -= 1;
+                                propositions.append(proposition);
+                                    
+                                if (proposition > nombreMystere) {
+                                    indications = "Votre proposition est trop grande";
+                                } else if (proposition < nombreMystere) {
+                                    indications = "Votre proposition est trop petite";
+                                } else {
+                                    indications = "Gagné !";
+                                }
+                                    
+                                if (nbTentatives == 0 || indications == "Gagné !") {
+                                    indications = finDePartie(nombreTentatives: nbTentatives, reponse: nombreMystere);
+                                }
+                            }
+                        } else {
+                            indications = "Votre proposition est incompréhensible...";
+                        }
+                        }.foregroundColor(Color.white)
+                .disabled(nbTentatives == 0 || indications == "Gagné !")
+                .padding()
+                .fontWeight(.bold)
+                .frame(maxWidth: 250)
+                .background((nbTentatives == 0 || indications == "Gagné !") ? Color.gray : Color.blue)
+                
+                        Button("Nouvelle partie") {
+                            nombreMystere = Int.random(in: 1...100);
+                            nbJoue = "";
+                            indications = "";
+                            nbTentatives = 10;
+                            propositions.removeAll();
+                        }.foregroundColor(Color.white)
+                                    .padding()
+                                    .fontWeight(.bold)
+                                    .frame(maxWidth: 250).background(Color.blue)
+                    }.frame(maxWidth: 350)
+                            .padding()
                     
-                    if (proposition > nombreMystere) {
-                        indications = "Votre proposition est trop grande";
-                    } else if (proposition < nombreMystere) {
-                        indications = "Votre proposition est trop petite"
-                    } else {
-                        indications = "Gagné !"
-                    }
-                }.padding()
-                
                 Spacer()
-                VStack {
-                    Text("Indications : \(indications)")
-                        .padding()
-                }
-                    .foregroundColor(.white)
-                    .frame(
-                        maxWidth: .infinity,
-                        maxHeight: 100,
-                        alignment: .top
-                    )
+                
+                VStack(alignment: .center) {
+                    Text("\(indications)").padding()
+                }.foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 100, alignment: .top)
                     .background(Color.blue)
             }
             
-        }// frame qui couvre tout l'écran, en tenant compte des bordures de l'écran
+        } // frame qui couvre tout l'écran, en tenant compte des bordures de l'écran
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
     }
 }
