7720d3a10bb53e1fe24aec094de00661f6e2fdfd
Guillaume Hostache <guillaume.hostache@outlook.fr>
2024-10-15 14:41:10 +0200

    fin calculatrice

--- calculatrice-polonaise/calculatrice-polonaise/ViewController.swift
+++ calculatrice-polonaise/calculatrice-polonaise/ViewController.swift
@@ -21,86 +21,120 @@ class ViewController: UIViewController
     
     @IBOutlet var fifthLine: UILabel!
     
+    @IBOutlet var ErrorDisplayLabel: UILabel!
+    
     var myStack :StackRPN = StackRPN()
     
     var myCurrentInput :String = ""
     var hasPoint :Bool = false
-    let formatter = NumberFormatter()
+
+    @IBAction func clicSwap(_ sender: Any) {
+        print("DEBUG - clicSwap")
+        if (myStack.myArray.count >= 2) {
+            myStack.myArray.swapAt(0, 1)
+        } else {
+            ErrorDisplayLabel.text = "swap impossible"
+        }
+        displayStack()
+    }
     
     @IBAction func clicDigit(_ sender: Any) {
+        print("DEBUG - clicDigit")
+        ErrorDisplayLabel.text = ""
         let button :UIButton = sender as! UIButton;
-        print("Clique sur la touche \(button.tag)")
         myCurrentInput += "\(button.tag)"
         myInputDisplayLabel.text = myCurrentInput
     }
     
     @IBAction func clicEnter(_ sender: Any) {
-        print("Clique sur la touche Enter")
+        print("DEBUG - clicEnter")
+        ErrorDisplayLabel.text = ""
         
         hasPoint = false
-        let input :NSNumber = formatter.number(from: myCurrentInput)!
-        print(input)
-        myStack.push(aNumber: input)
-        print(myStack.toString() ?? "0")
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 3
+        formatter.decimalSeparator = "."
+        
+        let input :NSNumber? = formatter.number(from: myCurrentInput)
+        if (input != nil) {
+            myStack.push(aNumber: input!)
+        }
+        
         displayStack()
         myInputDisplayLabel.text = ""
         myCurrentInput = ""
     }
     
     @IBAction func clicDrop(_ sender: Any) {
-        print("Clique sur la touche Drop")
-        myStack.pop()
+        print("DEBUG - clicDrop")
+        ErrorDisplayLabel.text = ""
+        let _ :NSNumber? = myStack.pop()
+        displayStack()
     }
     
     @IBAction func clicDiv(_ sender: Any) {
-        print("Clique sur la touche Division (/)")
+        print("DEBUG - clicDiv")
+        ErrorDisplayLabel.text = ""
         let a :Double? = myStack.pop() as? Double
         let b :Double? = myStack.pop() as? Double
         if ((a != nil) && (b != nil)) {
             if (b != 0) {
                 let c :Double = a! / b!
                 myStack.push(aNumber: c as NSNumber)
+            } else {
+                ErrorDisplayLabel.text = "Division par 0 impossible"
             }
+        } else {
+            ErrorDisplayLabel.text = "Division impossible sans opérateurs"
         }
         displayStack()
     }
     
     @IBAction func clicMul(_ sender: Any) {
-        print("Clique sur la touche Multiplication (*)")
+        print("DEBUG - clicMul")
         let a :Double? = myStack.pop() as? Double
         let b :Double? = myStack.pop() as? Double
         if ((a != nil) && (b != nil)) {
             let c :Double = a! * b!
             myStack.push(aNumber: c as NSNumber)
+        } else {
+            ErrorDisplayLabel.text = "Impossible de faire la multiplication"
         }
         displayStack()
     }
     
     @IBAction func clicAdd(_ sender: Any) {
-        print("Clique sur la touche Addition (+)")
+        print("DEBUG - clicAdd")
         let a :Double? = myStack.pop() as? Double
         let b :Double? = myStack.pop() as? Double
         if ((a != nil) && (b != nil)) {
             let c :Double = a! + b!
             myStack.push(aNumber: c as NSNumber)
+        } else {
+            ErrorDisplayLabel.text = "Impossible de faire l'addition"
         }
         displayStack()
         
     }
     
     @IBAction func clicMinus(_ sender: Any) {
-        print("Clique sur la touche Soustraction (-)")
+        print("DEBUG - clicMinus")
         let a :Double? = myStack.pop() as? Double
         let b :Double? = myStack.pop() as? Double
         if ((a != nil) && (b != nil)) {
             let c :Double = a! - b!
             myStack.push(aNumber: c as NSNumber)
+        } else {
+            ErrorDisplayLabel.text = "Impossible de faire la soustraction"
         }
         displayStack()
     }
     
     @IBAction func clicPoint(_ sender: Any) {
-        print("Clique sur la touche Point (.)")
+        print("DEBUG - clicPoint")
+        ErrorDisplayLabel.text = ""
         if (!hasPoint) {
             myCurrentInput += "."
             myInputDisplayLabel.text = myCurrentInput
@@ -109,6 +143,7 @@ class ViewController: UIViewController
     }
     
     func displayStack() {
+        print("DEBUG - displayStack")
         let first :NSNumber? = myStack.getElementWith(aShift: 0)
         let second :NSNumber? = myStack.getElementWith(aShift: 1)
         let third :NSNumber? = myStack.getElementWith(aShift: 2)
@@ -129,10 +164,6 @@ class ViewController: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        formatter.numberStyle = .decimal
-        formatter.maximumFractionDigits = 6
-        formatter.decimalSeparator = "."
     }
     
 }
