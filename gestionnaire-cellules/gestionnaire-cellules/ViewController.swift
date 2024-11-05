1769a208069c6ebb0448982ebe801b8d0934e76e
Guillaume Hostache <guillaume.hostache@outlook.fr>
2024-11-05 13:16:59 +0100

    tp suite

--- gestionnaire-cellules/gestionnaire-cellules/ViewController.swift
+++ gestionnaire-cellules/gestionnaire-cellules/ViewController.swift
@@ -8,18 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-  
     
     var monTabEtudiant :[Etudiant] = []
     
+    @IBOutlet var maTableView: UITableView!
+    
     func creerTableauEtudiant() {
-        
+        for i in 0...5 {
+            var etudiant :Etudiant = Etudiant(nom: "Hostache", prenom: "Guillaume", formation: "M2 CIM", groupeTP: "DT")
+            
+            monTabEtudiant.append(etudiant)
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        creerTableauEtudiant()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return monTabEtudiant.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // return a cell for each row
+        let aCell = tableView.dequeueReusableCell(withIdentifier: "idCelluleEtudiant", for: indexPath) as! CelluleEtudiant
+        return aCell
         
     }
-
-
 }
